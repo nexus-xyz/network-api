c3b8e145645a411662854aa1aa8ef540a3543846
Collin Jackson <github@collinjackson.com>
2024-09-05 01:41:12 -0700

    Initial install script

--- public/install.sh
+++ public/install.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 
-echo "Hello world!"
+rustc --version || curl https://sh.rustup.rs -sSf | sh
+NEXUS_HOME=$HOME/.nexus
+if [ -d "$NEXUS_HOME" ]; then
+  echo "$NEXUS_HOME exists. Updating.";
+  (cd $NEXUS_HOME && git pull)
+else
+  git clone git@github.com:nexus-xyz/network-cli --branch @collinjackson/proving $NEXUS_HOME
+fi
+
+(cd $NEXUS_HOME/clients/cli && cargo run --release --bin prover -- dev.orchestrator.nexus.xyz)
