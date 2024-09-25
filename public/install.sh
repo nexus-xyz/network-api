8607fa91c679cf803e22d97633b67d70ecb549d6
Collin Jackson <collin@nexus.xyz>
2024-09-25 09:03:20 -0700

    feat: Install via zip (#3)

--- public/install.sh
+++ public/install.sh
@@ -2,13 +2,18 @@
 
 rustc --version || curl https://sh.rustup.rs -sSf | sh
 NEXUS_HOME=$HOME/.nexus
+CLI_ZIP=/tmp/nexus-network-api.zip
+curl -L --verbose "https://docs.google.com/uc?export=download&id=1kcbEeKpVEyvIqL-_cgR5sYdZe_fOEPs6" > $CLI_ZIP
 if [ -d "$NEXUS_HOME" ]; then
   echo "$NEXUS_HOME exists. Updating.";
-  (cd $NEXUS_HOME && git pull)
+  (cd $NEXUS_HOME && rm -rf network-api && unzip $CLI_ZIP)
+  # TODO: Once GitHub repo is public, do this instead
+  # (cd $NEXUS_HOME && git pull)
 else
-  git clone git@github.com:nexus-xyz/network-cli --branch @collinjackson/proving $NEXUS_HOME
+  # TODO: Once GitHub repo is public, do this instead
+  # git clone git@github.com:nexus-xyz/network-cli $NEXUS_HOME
+  mkdir -p $NEXUS_HOME
+  (cd $NEXUS_HOME && unzip $CLI_ZIP)
 fi
 
-# Note: Hostname will default to `orchestrator.nexus.xyz` in the public beta release.
-
-(cd $NEXUS_HOME/clients/cli && cargo run --release --bin prover -- dev.orchestrator.nexus.xyz)
+(cd $NEXUS_HOME/network-api/clients/cli && cargo run --release --bin prover -- beta.orchestrator.nexus.xyz)
