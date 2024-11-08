0bbf07728d5d755138c9b9e18adae3b20dead1a0
Collin Jackson <github@collinjackson.com>
2024-11-08 13:15:13 -0800

    add ci

--- proto/generate_protobufs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-mkdir -p clients/flutter/lib/src/generated
-protoc --experimental_allow_proto3_optional --dart_out=grpc:clients/flutter/lib/src/generated -Iproto proto/orchestrator.proto
-dart format clients/flutter/lib/src/generated
-(cd clients/dummy_client && cargo build || echo clients/dummy_client not found, possibly due to a sparse checkout.)
-(cd clients/cli && cargo build || echo clients/cli not found, possibly due to a sparse checkout.)
-(cd orchestrator && cargo build || echo orchestrator/ not found, possibly due a sparse checkout.)
