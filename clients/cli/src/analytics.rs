e08439696923638a01253c543f50254d986951e5
Diego Prats <diego.prats@gmail.com>
2024-11-12 17:03:59 -0800

    feat: use firebase instead of mixpanel in for event tracking (#32)
    Co-authored-by: Diego Prats <diegoprats@Diegos-MacBook-Pro-m3.local>
    Co-authored-by: Collin Jackson <collin@nexus.xyz>

--- clients/cli/src/analytics.rs
+++ clients/cli/src/analytics.rs
@@ -1,4 +1,4 @@
-use crate::config::analytics_token;
+use crate::config::{analytics_id, analytics_api_key};
 use chrono::Datelike;
 use chrono::Timelike;
 use reqwest::header::{ACCEPT, CONTENT_TYPE};
@@ -16,14 +16,27 @@ pub fn track(
 ) {
     println!("{}", description);
 
-    let token = analytics_token(ws_addr_string);
-    if token.is_empty() {
+    let firebase_app_id = analytics_id(ws_addr_string);
+    let firebase_api_key = analytics_api_key(ws_addr_string);
+    if firebase_app_id.is_empty() {
         return;
     }
     let local_now = chrono::offset::Local::now();
+
+    // For tracking events, we use the Firebase Measurement Protocol
+    // Firebase is mostly designed for mobile and web apps, but for our use case of a CLI,
+    // we can use the Measurement Protocol to track events by POST to a URL. 
+    // The only thing that may be unexpected is that the URL we use includes a firebase key
+
+    // Firebase format for properties for Measurement protocol: 
+    // https://developers.google.com/analytics/devguides/collection/protocol/ga4/reference?client_type=firebase#payload
+    // https://developers.google.com/analytics/devguides/collection/protocol/ga4/reference?client_type=firebase#payload_query_parameters
     let mut properties = json!({
-        "token": token,
         "time": SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_millis(),
+        // app_instance_id is the standard key Firebase uses this key to track the same user across sessions
+        // It is a bit redundant, but I wanted to keep the recommended format Firebase uses to minimize surprises
+        // I still left the distinct_id key as well for backwards compatibility
+        "app_instance_id": event_properties["prover_id"],  
         "distinct_id": event_properties["prover_id"],
         "prover_type": "volunteer",
         "client_type": "cli",
@@ -36,14 +49,26 @@ pub fn track(
     for (k, v) in event_properties.as_object().unwrap() {
         properties[k] = v.clone();
     }
+
+    // Firebase format for events
     let body = json!({
-        "event": event_name,
-        "properties": properties
+        "app_instance_id": event_properties["prover_id"],
+        "events": [{
+            "name": event_name,
+            "params": properties
+        }],
     });
+
     tokio::spawn(async move {
         let client = reqwest::Client::new();
+
         let _ = client
-            .post("https://api.mixpanel.com/track?ip=1")
+            // URL is the Google Analytics endpoint for Firebase: https://stackoverflow.com/questions/50355752/firebase-analytics-from-remote-rest-api
+            .post(format!(
+                "https://www.google-analytics.com/mp/collect?firebase_app_id={}&api_secret={}",
+                firebase_app_id,
+                firebase_api_key
+            ))
             .body(format!("[{}]", body))
             .header(ACCEPT, "text/plain")
             .header(CONTENT_TYPE, "application/json")
