5e17d5f45d094a0ec8f6d2db5feede7213c44215
Diego Prats <diegoprats@Diegos-MacBook-Pro-m3.local>
2024-11-19 20:55:49 -0800

    remove websockets code

--- clients/cli/src/network/websocket.rs
+++ clients/cli/src/network/websocket.rs
@@ -1,3 +1,4 @@
+//This module contains the logic for receiving messages from the orchestrator
+//Currently, it is not used for anything but could be useful in the future
 #[cfg(test)]
-mod tests {
-}
+mod tests {}
