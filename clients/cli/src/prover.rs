416afe1f5b553f35b4ad50fca102c460225b6721
Alec H <shuochenhuang93@gmail.com>
2024-11-19 16:36:23 -0800

    feat: proof progress writes for client side proving (#43)

--- clients/cli/src/prover.rs
+++ clients/cli/src/prover.rs
@@ -132,37 +132,6 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             end = total_steps
         }
 
-        let initial_progress = ClientProgramProofRequest {
-            steps_in_trace: total_steps as i32,
-            steps_proven: 0,
-            step_to_start: start as i32,
-            program_id: String::new(),      // TODO: pass program id
-            client_id_token: String::new(), // TODO: pass client id token
-            proof_duration_millis: 0,
-            proof_speed_hz: 0.0,
-        };
-
-        // Send with error handling
-        if let Err(e) = client
-            .send(Message::Binary(initial_progress.encode_to_vec()))
-            .await
-        {
-            eprintln!("Failed to send progress update: {}", e);
-            track(
-                "send_error".into(),
-                format!("Failed to send progress message: {}", e),
-                &ws_addr_string,
-                json!({
-                    "prover_id": prover_id,
-                    "error": e.to_string(),
-                }),
-            );
-            // TODO: Depending on error type, might want to:
-            // 1. Try to reconnect
-            // 2. Return error
-            // 3. Continue with next operation
-        }
-
         let z_st = tr.input(start).expect("error starting circuit trace");
         let mut proof = IVCProof::new(&z_st);
 
@@ -199,10 +168,11 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                 steps_in_trace: total_steps as i32,
                 steps_proven,
                 step_to_start: start as i32,
-                program_id: String::new(),      // TODO: pass program id
-                client_id_token: String::new(), // TODO: pass client id token
-                proof_duration_millis: progress_duration.as_millis() as i32, // TODO: find proof_duration_millis
-                proof_speed_hz: proof_cycles_hertz as f32, //TODO: find proof_cycles_hertz
+                program_id: "fast-fib".to_string(),
+                client_id_token: None,
+                proof_duration_millis: progress_duration.as_millis() as i32,
+                k,
+                cli_prover_id: Some(prover_id.clone()),
             };
 
             track(
