0c758079ec185bd79cfcc2df99b0ba34af7ff5aa
Collin Jackson <collin@nexus.xyz>
2024-12-07 20:35:19 -0800

    docs: explain assertions on experimentation (#83)

--- clients/cli/src/utils/prover.rs
+++ clients/cli/src/utils/prover.rs
@@ -1,7 +1,10 @@
 use crate::utils::experiment::Experiment;
 
 pub fn get_program_for_prover(prover_id: &str) -> String {
-    // Ensure md5 is being used
+    // For an experiment with 1% enrollment, the first 53 provers
+    // are not enrolled and then we get an enrolled one, when
+    // md5 is being used. A few assertions here ensure that the
+    // algorithm is producing results that match web.
     debug_assert!(!Experiment::CANCER_DIAGNOSTIC.is_enrolled("52"));
     debug_assert!(Experiment::CANCER_DIAGNOSTIC.is_enrolled("53"));
     let program_name = if Experiment::CANCER_DIAGNOSTIC.is_enrolled(prover_id) {
