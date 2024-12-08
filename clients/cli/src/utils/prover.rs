57fc049fe5d2a00368c53631be1c21e0318e11f1
Collin Jackson <github@collinjackson.com>
2024-12-07 20:02:42 -0800

    Debug assert

--- clients/cli/src/utils/prover.rs
+++ clients/cli/src/utils/prover.rs
@@ -1,7 +1,9 @@
 use crate::utils::experiment::Experiment;
 
 pub fn get_program_for_prover(prover_id: &str) -> String {
-    // If hash mod 100 falls below percentage, select cancer-diagnostic
+    // Ensure md5 is being used
+    debug_assert!(!Experiment::CANCER_DIAGNOSTIC.is_enrolled("52"));
+    debug_assert!(Experiment::CANCER_DIAGNOSTIC.is_enrolled("53"));
     let program_name = if Experiment::CANCER_DIAGNOSTIC.is_enrolled(prover_id) {
         "cancer-diagnostic" // Selected with CANCER_DIAGNOSTIC_PERCENTAGE chance
     } else {
