dd3e50f0442ec99fadd912627b1ad6d213f52acc
Diego Prats <diego.prats@gmail.com>
2024-12-12 10:44:56 -0800

    update build file and cargo toml so it does not use protoc to generate protobufs

--- clients/cli/build.rs
+++ clients/cli/build.rs
@@ -1,29 +1,16 @@
-use std::{error::Error, path::PathBuf, process::Command};
+use std::{error::Error, path::PathBuf};
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let out_dir: PathBuf = "./src/generated/".into();
-    let proto_file: PathBuf = "../../proto/orchestrator.proto".into();
-    let proto_dir = match proto_file.parent().ok_or("Failed to get parent directory of proto file") {
-        Ok(dir) => dir,
-        Err(e) => return Err(e.into()),
-    };
+    let generated_file: PathBuf = "./src/generated/nexus.orchestrator.rs".into();
 
-    match Command::new("protoc")
-        .arg("--version")
-        .output() { 
-        Ok(_) => prost_build::Config::new()
-            .out_dir(out_dir)
-            .protoc_arg("--experimental_allow_proto3_optional")
-            .compile_protos(&[&proto_file], &[proto_dir])?,
-        Err(e) => {
-            // Skipping protobuf compilation.
-            println!("cargo:warning=Failed to run protoc: {}", e);
-            return Err(e.into());
-        }
+    // Verify the generated file exists
+    if !generated_file.exists() {
+        println!(
+            "cargo:warning=Generated protobuf file not found at {}",
+            generated_file.display()
+        );
+        return Err("Missing required generated protobuf file".into());
     }
-
-     // Tell Cargo to rerun this script if the proto file changes
-     println!("cargo:rerun-if-changed={}", proto_file.display());
 
     Ok(())
 }
