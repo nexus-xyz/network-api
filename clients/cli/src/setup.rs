9e03ba47ffdb8bcbb460cb1494c84c16efd4357e
Yae Miko <74785477+Sakura-nee@users.noreply.github.com>
2025-02-19 17:54:23 +0700

    Update setup.rs

--- clients/cli/src/setup.rs
+++ clients/cli/src/setup.rs
@@ -85,11 +85,15 @@ pub async fn run_initial_setup() -> SetupResult {
         );
 
         //ask the user if they want to use the existing config
-        println!("Do you want to use the existing user account? (y/n)");
-        let mut use_existing_config = String::new();
+        println!("Do you want to use the existing user account? (y/n) y");
+        let mut use_existing_config = "y" // String::new();
+
+        /*
         std::io::stdin()
             .read_line(&mut use_existing_config)
             .unwrap();
+        */
+            
         let use_existing_config = use_existing_config.trim();
         if use_existing_config == "y" {
             match fs::read_to_string(&node_id_path) {
